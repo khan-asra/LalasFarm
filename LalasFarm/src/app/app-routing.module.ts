8e58deabe7699bd216a68cc45dd59d823433d941
khan-asra <khanasr@sheridancollege.ca>
2023-10-20 00:05:33 -0400

    routing added

--- LalasFarm/src/app/app-routing.module.ts
+++ LalasFarm/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { OfferComponent } from './components/offer/offer.component';
+import { ContactUsComponent } from './components/contact-us/contact-us.component';
+import { StoryComponent } from './components/story/story.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component:  NavBarComponent },
+  { path: 'offer', component:  OfferComponent},
+  { path: 'contactUs', component:  ContactUsComponent },
+  { path: 'Story', component:  StoryComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- LalasFarm/src/app/app.component.ts
+++ LalasFarm/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'LalasFarm';
+
+
+
+
+
+  prepareRoute(outlet: RouterOutlet) {
+    return outlet && outlet.activatedRouteData
+
+    // && outlet.activatedRouteData['animation']
+  }
 }
