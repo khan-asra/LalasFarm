6e569b97be76293ab11c5b1f7cfa159d437a6701
khan-asra <khanasr@sheridancollege.ca>
2023-10-19 23:22:13 -0400

    components added

--- LalasFarm/src/app/app.module.ts
+++ LalasFarm/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { StoryComponent } from './components/story/story.component';
+import { OfferComponent } from './components/offer/offer.component';
+import { ContactUsComponent } from './components/contact-us/contact-us.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    StoryComponent,
+    OfferComponent,
+    ContactUsComponent
   ],
   imports: [
     BrowserModule,
